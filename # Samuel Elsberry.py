f34f735197813205b7487ba76fbb1ed4d613fee4
Sam-Elsberry <jackthefloof505@gmail.com>
2025-09-23 13:46:12 -0400

    Add files via upload

---
+++ # Samuel Elsberry.py
@@ -0,0 +1,13 @@
+# Samuel Elsberry
+# Period 8
+# 9/23/2025
+
+first_name = input("What is your first name?")
+last_name = input("What is your last name?")
+print()
+print()
+print("Welcome", first_name, last_name, "!")
+age = input("How old are you?")
+print()
+print()
+print("Wow,", first_name, last_name, "you are", age, "years old!")
